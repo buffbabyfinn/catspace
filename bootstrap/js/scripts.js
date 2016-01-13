ed6ba4626772ed0aec9f7d246eeed375d298a4c6
jon sweeney and megan fayer <js>
2016-01-12 16:24:56 -0800

    Choice of layout submit form.

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,17 +1,26 @@
 $(function() {
   $('#blanks form').submit(function(event) {
     var nameInput = $("input#name").val();
-    var prizeInput = $("input#prize").val();
-    var amountInput = $("input#amount").val();
-    var locationInput = $("input#location").val();
+    var numMiceInput = $("input#numMice").val();
+    var phoneInput = $("input#phone").val();
+    var paymentInput = $("input#payment").val();
 
     $('.name').text(nameInput);
-    $('.prize').text(prizeInput);
-    $('.amount').text(amountInput);
-    $('.location').text(locationInput);
+    $('.numMice').text(numMiceInput);
+    $('.phone').text(phoneInput);
+    $('.payment').text(paymentInput);
 
     $('#letter').show();
 
     event.preventDefault();
+  });
+  $("button#futureCat").click(function(){
+    $(".content").addClass("layout1");
+      });
+  $("button#futureCat").click(function(){
+    $(".content").addClass("layout2");
+      });
+  $("button#futureCat").click(function(){
+    $(".content").addClass("layout3");
   });
 });
